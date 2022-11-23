ae6a5b3395194d355f8563abbc81a5dffeefbdad
benx13 <32802278+benx13@users.noreply.github.com>
2022-11-23 23:59:46 +0100

    fixed xgboost

--- pages/machine_learning.py
+++ pages/machine_learning.py
@@ -5,9 +5,20 @@ import cv2
 import numpy as np
 import pickle
 from skimage.feature import hog
+import matplotlib.pyplot as plt
 
-if 'xgboost' not in st.session_state:
-    st.session_state.xgboost = pickle.load(open("/home/benx13/code/ocr/ocr_image_classifier/models/xgboost-hog.pkl", 'rb'))
+if 'xgboost_hog' not in st.session_state:
+    st.session_state.xgboost_hog = pickle.load(open("/home/benx13/code/ocr/ocr_image_classifier/models/xgboost-hog.pkl", 'rb'))
+if 'xgboost_raw' not in st.session_state:
+    st.session_state.xgboost_raw = pickle.load(open("/home/benx13/code/ocr/ocr_image_classifier/models/xgboost-raw.pkl", 'rb'))
+if 'xgboost_pr' not in st.session_state:
+    st.session_state.xgboost_pr = pickle.load(open("/home/benx13/code/ocr/ocr_image_classifier/models/xgboost-pr.pkl", 'rb'))
+if 'ss_hog' not in st.session_state:
+    st.session_state.ss_hog = pickle.load(open("/home/benx13/code/ocr/ocr_image_classifier/models/ss-hog.pkl", 'rb'))
+if 'ss_raw' not in st.session_state:
+    st.session_state.ss_raw = pickle.load(open("/home/benx13/code/ocr/ocr_image_classifier/models/ss-raw.pkl", 'rb'))
+if 'ss_pr' not in st.session_state:
+    st.session_state.ss_pr = pickle.load(open("/home/benx13/code/ocr/ocr_image_classifier/models/ss-pr.pkl", 'rb'))
 
 st.set_page_config(layout='wide')
 st.title('Words classifier Machine Learning')
@@ -25,16 +36,45 @@ else:
 st.image(cv2.imread(name, 0), width=500)
 
 
-
-preprocess = st.sidebar.selectbox(
-    'Please select a feature extraction method?',
-    ('Projections', 'Raw images', "histogram of gradient"))
 modelname = st.sidebar.selectbox(
     'Please select a model?',
     ('XGBoost', "SVM"))
+
+if modelname == modelname == 'XGBoost':
+    preprocess = st.sidebar.selectbox(
+        'Please select a feature extraction method?',
+        ('Projections', 'Raw images', "histogram of gradient"))
+else:
+    preprocess = st.sidebar.selectbox(
+        'Please select a feature extraction method?',
+        ('Raw images', "countours"))
 if st.sidebar.button("Run"):
-    model = st.session_state['xgboost']
-    img = cv2.resize(cv2.imread(name, 0), (100, 33)).flatten()
-    st.write(img.shape)
-    features = hog(np.array(img), block_norm='L2-Hys')
-    st.write(features.shape)
+    if(modelname == 'XGBoost' and preprocess == 'histogram of gradient'):
+        model = st.session_state['xgboost_hog']
+        ss = st.session_state['ss_hog']
+        img = cv2.resize(cv2.imread(name, 0), (100, 33))
+        features, vis = hog(np.array(img), block_norm='L2-Hys',visualize=True)
+        st.write("Feature Map")
+        st.write(vis.shape)
+        st.image(vis/255, width=500)
+        prob = model.predict_proba(ss.transform(features.reshape(1, features.shape[0])))
+        st.write('probability')
+        st.write("predicted class: ", np.argmax(prob), ", probability: ",np.max(prob))
+    if(modelname == 'XGBoost' and preprocess == 'Raw images'):
+        model = st.session_state['xgboost_raw']
+        ss = st.session_state['ss_raw']
+        img = cv2.resize(cv2.imread(name, 0), (100, 33)).flatten()
+        prob = model.predict_proba(ss.transform(img.reshape(1, img.shape[0])))
+        st.write('probability')
+        st.write("predicted class: ", np.argmax(prob), ", probability: ",np.max(prob))
+    if(modelname == 'XGBoost' and preprocess == 'Projections'):
+        model = st.session_state['xgboost_pr']
+        ss = st.session_state['ss_pr']
+        img = np.sum(cv2.resize(cv2.imread(name, 0), (100, 33)), axis=0)
+        st.write(img.shape)
+        fig, x = plt.subplots()
+        x.plot(img)
+        st.pyplot(fig)
+        prob = model.predict_proba(ss.transform(img.reshape(1, img.shape[0])))
+        st.write('probability')
+        st.write("predicted class: ", np.argmax(prob), ", probability: ",np.max(prob))
